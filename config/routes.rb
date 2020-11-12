f613416ec7c5768a5e2476f476d246624fe0cfda
valeria furin <v.furin@web.de>
2020-11-12 16:59:39 +0100

    Created first routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get "cocktails/index"
-  get "cocktails/show"
-  get "cocktails/new"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :cocktails, only: [:index, :show, :new, :create]
 end
